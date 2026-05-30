1a17c5b1f834394df1f583c8c65779b7dcb9a705
PikuZheng <cba321123@gmail.com>
2026-05-30 08:51:57 +0800

    disable click to copy
    移除点击复制

--- src/components/dashboard/query-log/query-log-table.tsx
+++ src/components/dashboard/query-log/query-log-table.tsx
@@ -550,7 +550,7 @@ function TableQueryLogs(): React.JSX.Element {
     enableColumnOrdering: true,
     enableRowActions: true,
     enableCellActions: true,
-    enableClickToCopy: window.isSecureContext,
+    enableClickToCopy: false,
     enableGlobalFilter: false,
     columnFilterDisplayMode: 'popover',
     enableColumnFilterModes: true,
